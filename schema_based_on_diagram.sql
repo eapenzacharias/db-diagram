1962dd052543a795ab1fe1b9755a558deb2352a6
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-02-01 20:46:37 +0100

    Add tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -2,11 +2,26 @@ CREATE TABLE patients (
   id int GENERATED ALWAYS AS IDENTITY,
   name varchar(50),
   date_of_birth date
-)
+);
 
 CREATE TABLE medical_histories (
   id int GENERATED ALWAYS AS IDENTITY,
   admitted_at timestamp,
   patient_id int REFERENCES patients(id),
   status varchar(10)
-)
+);
+
+CREATE TABLE invoices (
+  id int GENERATED ALWAYS AS IDENTITY, 
+  total_amount int, 
+  generated_at timestamp, 
+  payed_at timestamp,
+  medical_history_id int REFERENCES medical_histories(id)
+);
+
+CREATE TABLE treatments (
+  id int GENERATED ALWAYS AS IDENTITY,
+  type varchar,
+  name varchar
+);
+
